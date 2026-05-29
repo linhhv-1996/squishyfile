e1c1ed9681bc950489d8c4e972c8ce037bd12ee7
Hoàng Linh <linhhv.196@gmail.com>
2026-05-29 18:44:59 +0700

    20260529-022

--- static/sw.js
+++ static/sw.js
@@ -2,13 +2,19 @@ self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (event) => {
+  const request = event.request.clone();
+
   event.respondWith(
-    fetch(event.request).then((res) => {
+    fetch(request).then((res) => {
       const headers = new Headers(res.headers);
       headers.set("Cross-Origin-Embedder-Policy", "require-corp");
       headers.set("Cross-Origin-Opener-Policy", "same-origin");
       headers.set("Cross-Origin-Resource-Policy", "cross-origin");
-      return new Response(res.body, { status: res.status, statusText: res.statusText, headers });
-    }).catch(() => fetch(event.request))
+      return new Response(res.body, {
+        status: res.status,
+        statusText: res.statusText,
+        headers,
+      });
+    }).catch(() => fetch(event.request.clone()))
   );
 });
